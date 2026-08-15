fb6137edda697cd8d595fb7e2f7d5f0bf80ce0a1
koushiky06 <yaramaddu.koushik@gmail.com>
2026-08-15 14:41:04 +0530

    Clean up test bench file by removing comments
    Removed header comments and unnecessary timescale directive.

--- test_bench_TB.v
+++ test_bench_TB.v
@@ -1,25 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.05.2026 13:46:32
-// Design Name: 
-// Module Name: test_bench_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-`timescale 1ns / 1ps
 
 module tb_single_cycle();
 
